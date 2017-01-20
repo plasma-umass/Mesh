d29e9b4c815de226edec4d1e9c4d7e2ec42ddc8f
Bobby Powers <bobbypowers@gmail.com>
2017-01-20 16:14:04 -0500

    groundwork for meshing

--- libmesh.cc
+++ libmesh.cc
@@ -1,61 +1,80 @@
 // Copyright 2016 University of Massachusetts, Amherst
 
-#include <stdlib.h>
+#include <stdio.h>   // for sprintf
+#include <stdlib.h>  // for abort
+#include <unistd.h>  // for write
+#include <cstddef>   // for size_t
+#include <new>       // for operator new
 
-#include <atomic>
+// never allocate exeecutable heap
+#define HL_MMAP_PROTECTION_MASK (PROT_READ | PROT_WRITE)
 
 #define MALLOC_TRACE 0
 
 #include "heaplayers.h"
+
+#include "file-backed-mmap.h"
+
 #include "wrappers/gnuwrapper.cpp"
 
 using namespace HL;
 
-template <class Mmap>
-class WrappedMmapAlloc : public Mmap {
+typedef ExactlyOneHeap<LockedHeap<PosixLockType, FreelistHeap<BumpAlloc<16384, PrivateFileBackedMmapHeap>>>>
+    InternalAlloc;
+
+class TopHeap : public ExactlyOneHeap<FileBackedMmapHeap<InternalAlloc>> {
  public:
-  enum { Alignment = Mmap::Alignment };
-
-  WrappedMmapAlloc() {
-    static char buf[2048];
-    auto len = sprintf(buf, "Mmap::Alignment: %d\n", Alignment);
-    write(1, buf, len);
-  }
-
-  void *malloc(size_t sz) {
-    return Mmap::map(sz);
-  }
-
-  void free(void *) {
-    // This should never get called.
-    abort();
-  }
+  typedef ExactlyOneHeap<FileBackedMmapHeap<InternalAlloc>> Super;
 };
 
-class TopHeap : public ExactlyOneHeap<SizeHeap<BumpAlloc<1048576, WrappedMmapAlloc<MmapWrapper>, 16>>> {
-  typedef ExactlyOneHeap<SizeHeap<BumpAlloc<1048576, WrappedMmapAlloc<MmapWrapper>, 16>>> Super;
+class MiniHeap {
+  enum { Alignment = 16 };  // FIXME
+
+  inline void *malloc(size_t sz) {
+    return nullptr;
+  }
+
+  inline size_t getSize(void *ptr) {
+    return 0;
+  }
+
+  inline void free(void *ptr) {
+  }
+
+  // rng
+  // bitmap
+};
+
+template <typename SuperHeap, typename InternalAlloc>
+class MeshingHeap : public SuperHeap {
+ public:
+  enum { Alignment = SuperHeap::Alignment };
 };
 
 // no freelists
-class MeshHeap : public ANSIWrapper<KingsleyHeap<TopHeap, TopHeap>> {};
+class CustomHeap : public ANSIWrapper<KingsleyHeap<MeshingHeap<TopHeap, InternalAlloc>, TopHeap>> {};
 
-inline static MeshHeap *getMeshHeap(void) {
-  static char mesh_buf[sizeof(MeshHeap)];
-  static MeshHeap *mesh = new (mesh_buf) MeshHeap();
-  return mesh;
+inline static CustomHeap *getCustomHeap(void) {
+  static char buf[sizeof(CustomHeap)];
+  static CustomHeap *heap = new (buf) CustomHeap();
+  return heap;
 }
+
+// want:
+
+// MiniHeapManager<MiniHeap, TopHeap>
 
 extern "C" {
 void *xxmalloc(size_t sz) {
-  return getMeshHeap()->malloc(sz);
+  return getCustomHeap()->malloc(sz);
 }
 
 void xxfree(void *ptr) {
-  getMeshHeap()->free(ptr);
+  getCustomHeap()->free(ptr);
 }
 
 size_t xxmalloc_usable_size(void *ptr) {
-  return getMeshHeap()->getSize(ptr);
+  return getCustomHeap()->getSize(ptr);
 }
 
 void xxmalloc_lock() {
