758eb93a6187105c56957943f7cf5f0a02d0b200
Bobby Powers <bobbypowers@gmail.com>
2017-02-10 14:19:16 -0500

    theory: add ability to check dump files in python

---
+++ theory/common.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+__all__ = ['ERROR', 'WARN', 'INFO', 'DEBUG', 'log', 'slurp']
+
+# from rainbow
+def make_reporter(verbosity, quiet, filelike):
+    '''
+    Returns a function suitible for logging use.
+    '''
+    if not quiet:
+
+        def report(level, msg, *args):
+            'Log if the specified severity is <= the initial verbosity.'
+            if level <= verbosity:
+                if len(args):
+                    filelike.write(msg % args + '\n')
+                else:
+                    filelike.write('%s\n' % (msg, ))
+    else:
+
+        def report(level, msg, *args):
+            '/dev/null logger.'
+            pass
+
+    return report
+
+
+ERROR = 0
+WARN = 1
+INFO = 2
+DEBUG = 3
+
+log = make_reporter(DEBUG, False, sys.stderr)
+
+def slurp(file_name):
+    '''
+    Reads in a file, stripping leading and trailing whitespace.
+    '''
+    with open(file_name, 'r') as f:
+        return f.read().strip()
