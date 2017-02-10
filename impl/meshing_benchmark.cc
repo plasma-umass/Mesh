d2572c3bf911c35d661b46ce411a2761b85e85b5
Bobby Powers <bobbypowers@gmail.com>
2017-02-10 11:54:10 -0500

    read in testcases

--- impl/meshing_benchmark.cc
+++ impl/meshing_benchmark.cc
@@ -1,7 +1,9 @@
 // Copyright 2016 University of Massachusetts, Amherst
 
 #include <cstdio>
+#include <cstring>
 #include <memory>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -16,27 +18,122 @@ using std::string;
 using std::unique_ptr;
 using std::make_unique;
 using mesh::Bitmap;
+using std::stoi;
 
 DEFINE_bool(v, false, "verbose debugging output");
+
+// https://stackoverflow.com/questions/236129/split-a-string-in-c
+template <typename Out>
+void split(const std::string &s, char delim, Out result) {
+  std::stringstream ss;
+  ss.str(s);
+  std::string item;
+  while (std::getline(ss, item, delim)) {
+    *(result++) = item;
+  }
+}
+
+// https://stackoverflow.com/questions/236129/split-a-string-in-c
+std::vector<std::string> split(const std::string &s, char delim) {
+  std::vector<std::string> elems;
+  split(s, delim, std::back_inserter(elems));
+  return elems;
+}
 
 class MeshTestcase {
 public:
   explicit MeshTestcase(int length, int occupancy, int nStrings, string method)
-      : length(length), occupancy(occupancy), nStrings(nStrings), method(method) {
+      : length(static_cast<size_t>(length)),
+        occupancy(static_cast<size_t>(occupancy)),
+        nStrings(static_cast<size_t>(nStrings)),
+        method(method) {
+    d_assert(length > 0);
+    d_assert(occupancy > 0);
+    d_assert(nStrings > 0);
   }
   vector<Bitmap<MallocHeap>> bitmaps{};
   vector<string> strings{};
 
-  int length;
-  int occupancy;
-  int nStrings;
+  size_t length;
+  size_t occupancy;
+  size_t nStrings;
   string method;
 
   int expectedResult{-1};
 };
 
 unique_ptr<MeshTestcase> openTestcase(const char *path) {
-  return make_unique<MeshTestcase>(-1, -1, -1, "unknown");
+  FILE *f = fopen(path, "r");
+  if (f == nullptr) {
+    fprintf(stderr, "ERROR: couldn't open testcase %s: %s\n", path, strerror(errno));
+    exit(1);
+  }
+
+  char *fname = strdup(basename(path));
+  char *dotPos = strrchr(fname, '.');
+  // remove the extension if it exists
+  if (dotPos) {
+    *dotPos = 0;
+  }
+
+  vector<string> parts = split(fname, ',');
+  if (parts.size() != 4) {
+    fprintf(stderr, "ERROR: expected filename to have 4 parts before extension: %s\n", path);
+    exit(1);
+  }
+  free(fname);
+
+  auto testcase = make_unique<MeshTestcase>(stoi(parts[0]), stoi(parts[1]), stoi(parts[2]), parts[3]);
+
+  bool loop = true;
+  while (loop) {
+    char *line = nullptr;
+    size_t n = 0;
+    ssize_t len = getline(&line, &n, f);
+    if ((len < 0 && !errno) || len == 0) {
+      if (line)
+        free(line);
+      fprintf(stderr, "ERROR: unexpected end of testcase\n");
+      exit(1);
+    } else if (len < 0) {
+      fprintf(stderr, "ERROR: getline: %s\n", strerror(errno));
+      exit(1);
+    }
+    d_assert(line != nullptr);
+
+    d_assert(len > 0);
+    if (line[len - 1] == '\n') {
+      line[len - 1] = 0;
+      len--;
+    }
+
+    d_assert(len > 0);
+    if (line[len - 1] == '\r') {
+      line[len - 1] = 0;
+      len--;
+    }
+
+    d_assert(len > 0);
+    if (line[0] == '-') {
+      testcase->expectedResult = stoi(&line[1]);
+      loop = false;
+    } else {
+      string sline{line};
+
+      testcase->bitmaps.emplace_back(sline);
+      testcase->strings.emplace_back(sline);
+
+      d_assert_msg(sline == testcase->bitmaps.back().to_string(sline.length()), "expected roundtrip '%s' == '%s'", line,
+                   testcase->bitmaps.back().to_string().c_str());
+    }
+    free(line);
+  }
+  fclose(f);
+
+  d_assert(testcase->strings.size() == testcase->length);
+  d_assert(testcase->expectedResult >= 0);
+
+  return testcase;
 }
 
 int main(int argc, char *argv[]) {
@@ -56,6 +153,7 @@ int main(int argc, char *argv[]) {
     if (FLAGS_v) {
       printf("meshing strings from %s\n", argv[i]);
     }
+    auto testcase = openTestcase(argv[i]);
   }
 
   return 0;
