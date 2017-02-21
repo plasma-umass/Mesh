8e24d6c8c071efc4c1eae343edbd19ab03a2006a
Bobby Powers <bobbypowers@gmail.com>
2017-02-21 16:04:27 -0500

    thread test source

---
+++ impl/test/thread.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+int g_i = 0;
+std::mutex g_i_mutex;  // protects g_i
+
+void safe_increment() {
+  std::lock_guard<std::mutex> lock(g_i_mutex);
+  ++g_i;
+
+  std::cout << std::this_thread::get_id() << ": " << g_i << '\n';
+
+  // g_i_mutex is automatically released when lock
+  // goes out of scope
+}
+
+int main() {
+  std::cout << __func__ << ": " << g_i << '\n';
+
+  std::thread t1(safe_increment);
+  std::thread t2(safe_increment);
+
+  t1.join();
+  t2.join();
+
+  std::cout << __func__ << ": " << g_i << '\n';
+}
