055f6a081783ad9a5b0cafd6c2585d13bcd45176
Bobby Powers <bobbypowers@gmail.com>
2017-03-18 13:47:24 -0400

    refactoring to make meshing more sane + testable

--- src/unit/mesh.cc
+++ src/unit/mesh.cc
@@ -20,6 +20,8 @@ TEST(MeshTest, TryMesh) {
   MiniHeap *mh1 = gheap.allocMiniheap(StrLen);
   MiniHeap *mh2 = gheap.allocMiniheap(StrLen);
 
+  ASSERT_TRUE(mh1 != mh2);
+
   ASSERT_EQ(mh1->maxCount(), mh2->maxCount());
   ASSERT_EQ(mh1->maxCount(), ObjCount);
 
@@ -32,8 +34,8 @@ TEST(MeshTest, TryMesh) {
   memset(s1, 'A', StrLen);
   memset(s2, 'Z', StrLen);
 
-  s1[StrLen - 1] = '0';
-  s2[StrLen - 1] = '0';
+  s1[StrLen - 1] = 0;
+  s2[StrLen - 1] = 0;
 
   char *v1 = strdup(s1);
   char *v2 = strdup(s2);
@@ -41,5 +43,28 @@ TEST(MeshTest, TryMesh) {
   ASSERT_TRUE(strcmp(s1, v1) == 0);
   ASSERT_TRUE(strcmp(s2, v2) == 0);
 
-  // gheap.mesh(mh1, mh2);
+  gheap.mesh(mh1, mh2);
+
+  // mh2 is consumed by mesh call
+  ASSERT_EQ(mh2, nullptr);
+
+  ASSERT_TRUE(strcmp(s1, v1) == 0);
+  ASSERT_TRUE(strcmp(s2, v2) == 0);
+
+  char *s3 = s1 + (ObjCount - 1) * StrLen;
+
+  ASSERT_TRUE(strcmp(s2, s3) == 0);
+
+  s2[0] = 'b';
+  ASSERT_EQ(s3[0], 'b');
+
+  gheap.free(s1);
+  ASSERT_TRUE(!mh1->isEmpty());
+
+  gheap.free(s2);
+  // only safe because mh1 isn't "done"
+  ASSERT_TRUE(mh1->isEmpty());
+
+  write(-1, "ABOUT TO FREE\n", strlen("ABOUT TO FREE\n"));
+  gheap.freeMiniheap(mh1);
 }
