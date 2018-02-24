5ec1d1d70400b2b99e5eab15906b0df6a8f0d2a9
Bobby Powers <bobby@stripe.com>
2018-02-24 11:55:21 -0800

    experiment with mesh_malloc/mesh_free

--- src/common.h
+++ src/common.h
@@ -29,6 +29,7 @@ using std::mutex;
 #define ATTRIBUTE_HIDDEN __attribute__((visibility("hidden")))
 #define ATTRIBUTE_ALIGNED(s) __attribute__((aligned(s)))
 #define CACHELINE 64
+#define CACHELINE_ALIGNED_FN ATTRIBUTE_ALIGNED(CACHELINE)
 
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
   TypeName(const TypeName &);              \
