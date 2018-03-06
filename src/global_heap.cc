f58b139366f05d2080cdcf747daf9959ff553c3e
Bobby Powers <bobbypowers@gmail.com>
2018-03-05 20:36:09 -0500

    global heap: uninline malloc

--- src/global_heap.cc
+++ src/global_heap.cc
@@ -2,3 +2,15 @@
 // Copyright 2017 University of Massachusetts, Amherst
 
 #include "global_heap.h"
+
+namespace mesh {
+void *GlobalHeap::malloc(size_t sz) {
+#ifndef NDEBUG
+  if (unlikely(sz <= kMaxSize))
+    abort();
+#endif
+
+  std::lock_guard<std::mutex> lock(_bigMutex);
+  return _bigheap.malloc(sz);
+}
+}  // namespace mesh
