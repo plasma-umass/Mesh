83d7df2ed0746bd29f1a745c08bc11976e2f9b9d
Bobby Powers <bobbypowers@gmail.com>
2018-03-18 15:56:23 -0400

    test: add big-alloc.c (missed adding to git earlier)

---
+++ src/test/big-alloc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SZ 32816
+#define NOBJS 20
+
+volatile char *volatile var;
+
+int main() {
+  char *objects[NOBJS];
+  memset(objects, 0, sizeof(*objects) * NOBJS);
+
+  for (size_t i = 0; i < 500000; i++) {
+    var = malloc(SZ);
+    memset((char *)var, 0x55, SZ);
+    size_t off = i % NOBJS;
+    if (objects[off] != NULL)
+      free(objects[off]);
+    objects[off] = (char *)var;
+    var = NULL;
+  }
+
+  for (size_t i = 0; i < NOBJS; i++) {
+    if (objects[i] != NULL)
+      free(objects[i]);
+  }
+}
