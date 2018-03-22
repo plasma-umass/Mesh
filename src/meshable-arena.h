a64f42d07b6f61fb3f76a05e6c96d5353718f782
Bobby Powers <bobbypowers@gmail.com>
2018-03-21 21:54:53 -0400

    refactor the meshable area to start thinking in Spans

--- src/meshable-arena.h
+++ src/meshable-arena.h
@@ -43,6 +43,18 @@ enum PageType {
 };
 }  // namespace internal
 
+typedef uint32_t Offset;
+typedef uint32_t Length;
+
+struct Span {
+  // offset and length are in pages
+  explicit Span(Offset _offset, Length _length) : offset(_offset), length(_length) {
+  }
+
+  const Offset offset;
+  const Length length;
+};
+
 class MeshableArena : public mesh::OneWayMmapHeap {
 private:
   DISALLOW_COPY_AND_ASSIGN(MeshableArena);
@@ -59,116 +71,15 @@ public:
     return arena <= ptrval && ptrval < arena + kArenaSize;
   }
 
-  inline void *malloc(size_t sz) {
-    if (sz == 0)
-      return nullptr;
+private:
+  Span reservePages(Length pageCount);
+  void freeSpan(Span span);
+  void freePhys(void *ptr, size_t sz);
+  internal::Bitmap allocatedBitmap() const;
 
-    d_assert(_arenaBegin != nullptr);
-    d_assert_msg(sz % HL::CPUInfo::PageSize == 0, "multiple-page allocs only, sz bad: %zu", sz);
-
-    if (sz == HL::CPUInfo::PageSize) {
-      size_t off = _bitmap.setFirstEmpty();
-      setMetadata(off, internal::PageType::Identity);
-      return ptrFromOffset(off);
-    }
-
-    const auto pageCount = sz / HL::CPUInfo::PageSize;
-    d_assert(pageCount >= 2);
-    d_assert(pageCount <= 64);
-
-    uint64_t pattern = 0; // (1 << pageCount) - 1;
-    for (size_t i = 0; i < pageCount; i++) {
-      pattern |= 1 << i;
-    }
-
-    // get a view of the underlying bits as chars
-    auto bits = reinterpret_cast<const uint8_t *>(_bitmap.bits());
-    const auto bitCount = _bitmap.bitCount();
-
-    // debug("searching for %p\n", reinterpret_cast<void *>(pattern));
-
-    size_t byteOff = 0;
-    for (; byteOff < (bitCount - pageCount)/8; byteOff++) {
-      uint64_t longBits = *reinterpret_cast<const uint64_t *>(bits + byteOff);
-      // a contiguous run of pages is available
-      if (((~longBits) & pattern) == pattern) {
-        break;
-      }
-    }
-    
-    const size_t off = byteOff * 8;
-
-    d_assert(getMetadataFlags(off) == 0 && getMetadataPtr(off) == 0);
-
-    // now that we know they are available, set the empty pages to
-    // in-use.  This is safe because this whole function is called
-    // under the GlobalHeap lock, so there is no chance of concurrent
-    // modification between the loop above and the one below.
-    for (size_t i = 0; i < pageCount; i++) {
-      bool ok = _bitmap.tryToSet(off + i);
-      hard_assert(ok);
-
-      d_assert(getMetadataFlags(off + i) == 0 && getMetadataPtr(off + i) == 0);
-      setMetadata(off + i, internal::PageType::Identity);
-    }
-
-    void *ptr = ptrFromOffset(off);
-
-    if (kAdviseDump) {
-      madvise(ptr, sz, MADV_DODUMP);
-    }
-
-    return ptr;
-  }
-
-  inline void freePhys(void *ptr, size_t sz) {
-    d_assert(contains(ptr));
-    d_assert(sz > 0);
-
-    d_assert(sz / CPUInfo::PageSize > 0);
-    d_assert(sz % CPUInfo::PageSize == 0);
-
-    const off_t off = reinterpret_cast<char *>(ptr) - reinterpret_cast<char *>(_arenaBegin);
-#ifndef __APPLE__
-    int result = fallocate(_fd, FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE, off, sz);
-    d_assert(result == 0);
-#else
-#warning macOS version of fallocate goes here
-#endif
-  }
-
-  inline void free(void *ptr, size_t sz) {
-    if (unlikely(!contains(ptr))) {
-      return;
-    }
-    d_assert(sz > 0);
-
-    d_assert(sz / CPUInfo::PageSize > 0);
-    d_assert(sz % CPUInfo::PageSize == 0);
-
-    const auto off = offsetFor(ptr);
-    const uint8_t flags = getMetadataFlags(off);
-    if (flags == internal::PageType::Identity) {
-      madvise(ptr, sz, MADV_DONTNEED);
-      if (kAdviseDump) {
-        madvise(ptr, sz, MADV_DONTDUMP);
-      }
-      freePhys(ptr, sz);
-    } else {
-      // restore identity mapping
-      mmap(ptr, sz, HL_MMAP_PROTECTION_MASK, MAP_SHARED | MAP_FIXED, _fd, off * CPUInfo::PageSize);
-    }
-
-    const auto pageCount = sz / CPUInfo::PageSize;
-    for (size_t i = 0; i < pageCount; i++) {
-      d_assert(_bitmap.isSet(off + i));
-      // clear the miniheap pointers we were tracking
-      setMetadata(off + i, 0);
-      _bitmap.unset(off + i);
-    }
-
-    // debug("in use count after free of %p/%zu: %zu\n", ptr, sz, _bitmap.inUseCount());
-  }
+public:
+  void *malloc(size_t sz);
+  void free(void *ptr, size_t sz);
 
   inline void assoc(const void *span, void *miniheap, size_t pageCount) {
     if (unlikely(!contains(span))) {
@@ -202,10 +113,6 @@ public:
 
   void beginMesh(void *keep, void *remove, size_t sz);
   void finalizeMesh(void *keep, void *remove, size_t sz);
-
-  const internal::Bitmap &bitmap() const {
-    return _bitmap;
-  }
 
 private:
   static constexpr inline size_t metadataSize() {
@@ -267,8 +174,14 @@ private:
 
   void *_arenaBegin{nullptr};
 
-  // per-page bitmap
-  internal::Bitmap _bitmap;
+  // spans that had been meshed, have been freed, and need to be reset
+  // to identity mappings in the page tables.
+  internal::vector<Span> _toReset;
+
+  internal::vector<Span> _clean[kSpanClassCount];
+  internal::vector<Span> _dirty[kSpanClassCount];
+
+  Offset _end{};
 
   // indexed by offset
   atomic<uintptr_t> *_metadata{nullptr};
