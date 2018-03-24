32e2ce50c975576ec9db9bcd2b0bcd8f24aa983b
Bobby Powers <bobbypowers@gmail.com>
2018-03-24 15:04:34 -0400

    unit-tests: disable implicit meshing during explicit meshing tests

--- src/unit/mesh.cc
+++ src/unit/mesh.cc
@@ -22,6 +22,9 @@ static inline void note(const char *note) {
 static void meshTest(bool invert) {
   GlobalHeap &gheap = runtime().heap();
 
+  // disable automatic meshing for this test
+  gheap.setMeshPeriodSecs(-1);
+
   ASSERT_EQ(gheap.getAllocatedMiniheapCount(), 0UL);
 
   // allocate two miniheaps for the same object size from our global heap
