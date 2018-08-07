1a89555387224a6b2265b5278d54f632bc3cf46a
Bobby Powers <bobbypowers@gmail.com>
2018-08-06 22:40:13 -0700

    sigsegv handler and other updates

--- test/python/run.sh
+++ test/python/run.sh
@@ -2,10 +2,13 @@
 set -euo pipefail
 set -x
 
-TEST=big3.py
+TEST=fragmentation.py
+#TEST=big3.py
 #TEST=python_memleak.py
 
 /bin/time mstat -o glibc.tsv -freq 127 -env PYTHON_MALLOC=malloc python3 $TEST
+sleep 5
+/bin/time mstat -o glibc2.tsv -freq 127 -env PYTHON_MALLOC=malloc -env MALLOC_ARENA_MAX=2 python3 $TEST
 sleep 5
 /bin/time mstat -o pymalloc.tsv -freq 127 -env PYTHON_MALLOC=pymalloc python3 $TEST
 sleep 5
