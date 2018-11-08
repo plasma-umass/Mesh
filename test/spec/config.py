669927cfdb349ad9094c328b091d6ef3646225e3
Bobby Powers <bobbypowers@gmail.com>
2018-11-07 17:42:52 -0800

    work on speedometer + docker updates

--- test/spec/config.py
+++ test/spec/config.py
@@ -1,14 +1,21 @@
 import collections
 
 class Config:
-    def __init__(self, name, docker_image):
+    def __init__(self, name, docker_image, dir_name=None):
         self.name = name
         self.docker_image = docker_image
+        if dir_name:
+            self.dir_name = dir_name
+        else:
+            self.dir_name = name
 
 configs = [
-    Config('mesh', 'bpowers/spec:mesh-v2'),
-    Config('jemalloc', 'bpowers/spec:jemalloc'),
-    Config('glibc', 'bpowers/spec:glibc'),
-    Config('diehard', 'bpowers/spec:diehard'),
-    Config('hoard', 'bpowers/spec:hoard'),
+    # Config('mesh', 'bpowers/spec:mesh-0n', 'mesh-0n'),
+    # Config('mesh', 'bpowers/spec:mesh-2y', 'mesh-2y'),
+    Config('mesh', 'bpowers/spec:mesh-0y', 'mesh-0y'),
+    Config('mesh', 'bpowers/spec:mesh-1y', 'mesh-1y'),
+    # Config('jemalloc', 'bpowers/spec:jemalloc'),
+    # Config('glibc', 'bpowers/spec:glibc'),
+    # Config('diehard', 'bpowers/spec:diehard'),
+    # Config('hoard', 'bpowers/spec:hoard'),
 ]
