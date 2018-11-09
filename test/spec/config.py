64bfea05d71a9e087a353e6a4c92a11ede74fe0d
Bobby Powers <bobbypowers@gmail.com>
2018-11-09 08:13:26 -0800

    small test updates

--- test/spec/config.py
+++ test/spec/config.py
@@ -10,10 +10,10 @@ class Config:
             self.dir_name = name
 
 configs = [
-    # Config('mesh', 'bpowers/spec:mesh-0n', 'mesh-0n'),
-    # Config('mesh', 'bpowers/spec:mesh-2y', 'mesh-2y'),
-    Config('mesh', 'bpowers/spec:mesh-0y', 'mesh-0y'),
-    Config('mesh', 'bpowers/spec:mesh-1y', 'mesh-1y'),
+    Config('mesh', 'bpowers/spec:mesh-0n', 'mesh-0n'),
+    Config('mesh', 'bpowers/spec:mesh-2y', 'mesh-2y'),
+    # Config('mesh', 'bpowers/spec:mesh-0y', 'mesh-0y'),
+    # Config('mesh', 'bpowers/spec:mesh-1y', 'mesh-1y'),
     # Config('jemalloc', 'bpowers/spec:jemalloc'),
     # Config('glibc', 'bpowers/spec:glibc'),
     # Config('diehard', 'bpowers/spec:diehard'),
