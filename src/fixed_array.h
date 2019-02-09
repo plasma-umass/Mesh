c2a4e5c4f262e661462bc0291650dbc53582ef06
Bobby Powers <bobbypowers@gmail.com>
2019-02-09 12:25:58 -0800

    fixed_array: add a simple fixed-size array implementation

---
+++ src/fixed_array.h
@@ -0,0 +1,115 @@
+// -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil -*-
+// Copyright 2017 University of Massachusetts, Amherst
+
+#pragma once
+#ifndef MESH__FIXED_ARRAY_H
+#define MESH__FIXED_ARRAY_H
+
+#include <iterator>
+
+namespace mesh {
+
+// enables iteration through the miniheaps in an array
+template <typename FixedArray>
+class FixedArrayIter : public std::iterator<std::forward_iterator_tag, typename FixedArray::value_type> {
+public:
+  FixedArrayIter(const FixedArray &a, const size_t i) : _i(i), _array(a) {
+  }
+  FixedArrayIter &operator++() {
+    if (unlikely(_i + 1 >= _array.size())) {
+      _i = _array.size();
+      return *this;
+    }
+
+    _i++;
+    return *this;
+  }
+  bool operator==(const FixedArrayIter &rhs) const {
+    return &_array == &rhs._array && _i == rhs._i;
+  }
+  bool operator!=(const FixedArrayIter &rhs) const {
+    return &_array != &rhs._array || _i != rhs._i;
+  }
+  typename FixedArray::value_type operator*() {
+    return _array[_i];
+  }
+
+private:
+  size_t _i;
+  const FixedArray &_array;
+};
+
+template <typename T, size_t Capacity>
+class FixedArray {
+private:
+  DISALLOW_COPY_AND_ASSIGN(FixedArray);
+
+public:
+  typedef T *value_type;
+  typedef FixedArrayIter<FixedArray<T, Capacity>> iterator;
+  typedef FixedArrayIter<FixedArray<T, Capacity>> const const_iterator;
+
+  FixedArray() {
+    d_assert(_size == 0);
+#ifndef NDEBUG
+    for (size_t i = 0; i < Capacity; i++) {
+      d_assert(_objects[i] == nullptr);
+    }
+#endif
+  }
+
+  ~FixedArray() {
+    clear();
+  }
+
+  size_t size() const {
+    return _size;
+  }
+
+  bool full() const {
+    return _size == Capacity;
+  }
+
+  void clear() {
+    memset(_objects, 0, Capacity * sizeof(T *));
+    _size = 0;
+  }
+
+  void append(T *obj) {
+    hard_assert(_size < Capacity);
+    _objects[_size] = obj;
+    _size++;
+  }
+
+  T *operator[](size_t i) const {
+    d_assert(i < _size);
+    return _objects[i];
+  }
+
+  iterator begin() {
+    return iterator(*this, 0);
+  }
+  iterator end() {
+    return iterator(*this, size());
+  }
+  const_iterator begin() const {
+    return iterator(*this, 0);
+  }
+  const_iterator end() const {
+    return iterator(*this, size());
+  }
+  const_iterator cbegin() const {
+    return iterator(*this, 0);
+  }
+  const_iterator cend() const {
+    return iterator(*this, size());
+  }
+
+private:
+  size_t _size{0};
+  T *_objects[Capacity]{};
+};
+
+}  // namespace mesh
+
+#endif  // MESH__FIXED_ARRAY_H
